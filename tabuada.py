f07b2b0618fa2fb62fcf5471e96a229ef51b1e70
lavqueirozz <laviniasq5@gmail.com>
2024-09-15 20:05:46 -0300

    Calculos IMC

---
+++ tabuada.py
@@ -0,0 +1,4 @@
+print('Tabuada do numero 5')
+
+for numero in range (0,51,5):
+    print(numero, end= " ")
---
+++ teste.py
@@ -0,0 +1,10 @@
+estadosCidades= {
+    'São Paulo': {'cidades': 'Ribeirão Preto' 'São Paulo' 'Caraguatatuba'},
+    'Rio de Janeiro': {'cidades': 'Cabo Frio' 'Rio de janeiro'},
+
+}
+
+
+cidade = estadosCidades['São Paulo'] ['cidades']
+
+print (cidade)
